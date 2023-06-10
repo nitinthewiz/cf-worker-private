5be076f9b73fc106897b84c6d72c5d80ae64c20a
Nitin Khanna <mail@nitinkhanna.com>
2023-06-10 10:42:56 -0700

    first push

---
+++ index.js
@@ -0,0 +1,27 @@
+import { parse } from "cookie";
+
+addEventListener("fetch", (event) => {
+    event.respondWith(doRedirects(event.request));
+});
+
+const newLocationProtocol = "https://"
+const newLocationHost = "developers.cloudflare.com";
+const newLocationPath = "/workers/learning/"
+
+async function doRedirects(request) {
+    let reqUA = request.headers.get("user-agent");
+    if (reqUA.includes("curl")) {
+        const COOKIE_NAME = "cf-noredir";
+        const cookie = parse(request.headers.get("Cookie") || "");
+        if (cookie[COOKIE_NAME] != null) {
+            if (cookie[COOKIE_NAME] == "true"){
+                return Response.redirect(newLocationProtocol + newLocationHost, 302);
+            }
+        }
+        let newLocation = newLocationProtocol + newLocationHost + newLocationPath;
+        return Response.redirect(newLocation, 302);
+        // return fetch(newLocation);
+    }
+    // return fetch(request);
+    return Response.redirect(newLocationProtocol + newLocationHost, 302);
+}
